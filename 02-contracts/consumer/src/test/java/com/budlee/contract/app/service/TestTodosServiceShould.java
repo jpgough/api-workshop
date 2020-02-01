e36545e2e8b223129e31b13081dd48eacde47f22
MattyA <mat.auburn@gmail.com>
2020-02-01 19:35:36 +0000

    Upgrade to consumer libs

--- 02-contracts/consumer/src/test/java/com/budlee/contract/app/service/TestTodosServiceShould.java
+++ 02-contracts/consumer/src/test/java/com/budlee/contract/app/service/TestTodosServiceShould.java
@@ -1,29 +1,28 @@
 package com.budlee.contract.app.service;
 
-import au.com.dius.pact.consumer.Pact;
-import au.com.dius.pact.consumer.PactProviderRuleMk2;
-import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.model.RequestResponsePact;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.annotations.Pact;
 import com.budlee.contract.app.model.Todo;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "todo-api-producer-pact", port = "8083")
 public class TestTodosServiceShould {
-
-    @Rule
-    public PactProviderRuleMk2 mockProvider =
-            new PactProviderRuleMk2("todo-api-producer-pact", "localhost", 8083, this);
 
     @Pact(consumer = "todo-api-consumer-pact")
     public RequestResponsePact updateAToDoRequest(PactDslWithProvider builder) {
         return builder
-                .given("an todo of id 1 has already been created")
+                .given("an todo of id 0 has already been created")
                 .uponReceiving("a request to update the todo")
-                .matchPath("/todos/1")
+                .matchPath("/todos/0")
                 .method("PUT")
                 .headers("Content-Type", "application/json")
                 .body("{\"message\":\"Make all the beds\"}")
@@ -35,15 +34,15 @@ public class TestTodosServiceShould {
 
     TodosService todosService = new TodosService();
 
-    @Before
+    @BeforeEach
     public void setUp() {
     }
 
 
     @Test
-    @PactVerification
+    @PactTestFor
     public void updateTodo() {
         //NO CONTENT
-        assertEquals(this.todosService.updateTodo(1, new Todo("Make all the beds")).getStatusCodeValue(), 204);
+        assertEquals(this.todosService.updateTodo(0, new Todo("Make all the beds")).getStatusCodeValue(), 204);
     }
 }
